3436990524ea6c3c6ed1a1cc4ae68c1e7bd2ff81
Devan Barnard <YOUR_EMAIL@gmail.com>
2026-05-12 16:31:12 +0200

    Added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
+
 import App from './App.jsx'
+import Privacy from './Privacy.jsx'
+import Terms from './Terms.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
